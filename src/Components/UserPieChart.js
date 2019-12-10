b3f6adcc2cad78f8b027629eb0f3f55c84cacb10
CoanetPaul <paulcoanet@gmail.com>
2019-12-10 20:40:05 +0100

    Version final

--- src/Components/UserPieChart.js
+++ src/Components/UserPieChart.js
@@ -16,69 +16,53 @@ class UserPieChart extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            data01 : [
+            data : [
                 { name: 'bathroom', value: 1},
                 { name: 'bedroom', value: 1},
                 { name: 'entrance', value: 1 },
                 { name: 'livingroom', value: 1 }
             ],
-            datatamp : [
-                { name: 'bathroom', value: 0},
-                { name: 'bedroom', value: 0},
-                { name: 'entrance', value: 0 },
-                { name: 'livingroom', value: 0 }
-            ],
-            cptBathroom : 1,
-            cptBedroom : 1,
-            cptEntrance : 1,
-            cptLivingroom : 1,
         };
 
 
     }
 
-    componentWillMount() {
+
+    componentDidMount() {
         console.log("compo");
-            let api_sensor = new API_sensor();
-        api_sensor.fetchSensors().then(response => {
-            this.setState({allSensors: response.data});
-            for (var i = 0; i <= this.state.allSensors.length; i++) {
-                console.log("allSensors.location : ", this.state.allSensors[i].location);
-                if (this.state.allSensors[i].location === "bathroom") {
-                    this.state.datatamp[0].value = this.state.datatamp[0].value + 1;
+        let api_sensor = new API_sensor();
+        api_sensor
+            .fetchSensors()
+            .then(response => {
+                // je crée un tableau temporaire à partir dans mon state actuel
+                const tableauTemporaire = this.state.data;
+                this.setState({allSensors: response.data});
+                console.log("reponseeehhhe", this.state.allSensors[0].location);
+                for (let i = 0; i < this.state.allSensors.length; i++) {
+                    if (this.state.allSensors[i].location == "bathroom") {
+                        tableauTemporaire[0].value = tableauTemporaire[0].value + 1;
+                    }
+                    if (this.state.allSensors[i].location == "bedroom") {
+                        tableauTemporaire[1].value = tableauTemporaire[1].value + 1;
+                    }
+                    if (this.state.allSensors[i].location == "entrance") {
+                        tableauTemporaire[2].value = tableauTemporaire[2].value + 1;
+                    }
+                    if (this.state.allSensors[i].location == "livingroom") {
+                        tableauTemporaire[3].value = tableauTemporaire[3].value + 1;
+                    }
                 }
-                if (this.state.allSensors[i].location === "bedroom") {
-                    this.state.datatamp[1].value = this.state.datatamp[1].value + 1;
-                }
-                if (this.state.allSensors[i].location === "entrance") {
-                    this.state.datatamp[2].value = this.state.datatamp[2].value + 1;
-                }
-                if (this.state.allSensors[i].location === "livingroom") {
-                    this.state.datatamp[3].value = this.state.datatamp[3].value + 1;
-                }
-
-            }
-            this.setState({data01 : this.state.datatamp});
-            this.starte();
-        }).catch(onerror => {
-        });
-
+                this.setState({ data: tableauTemporaire });
+            })
+            .catch(error => {
+                console.error(error);
+            });
     }
-
-    starte(){
-        console.log("bwaaaaaaaaaaaaaaaaaaaaaaaaa");
-
-        console.log("CASE zero ", this.state.data01[0]);
-        console.log("CASE zero ", this.state.data01[1]);
-        console.log("CASE zero ", this.state.data01[2]);
-        console.log("CASE zero ", this.state.data01[3]);
-    }
-
 
     render() {
             return (
                 <PieChart width={300} height={300}>
-                    <Pie dataKey="value" isAnimationActive={true} data={this.state.data01} cx={150} cy={150} outerRadius={80} fill="#1FA4DF" label/>
+                    <Pie dataKey="value" isAnimationActive={true} data={this.state.data} cx={150} cy={150} outerRadius={80} fill="#1FA4DF" label/>
                     <Tooltip/>
                 </PieChart>
             );
--- src/Components/UserSensors.js
+++ src/Components/UserSensors.js
@@ -15,39 +15,61 @@ class UserSensors extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            totalUser : 0,
-            totalSensor : 0
+            boo : false,
+            totalSensor :0
         }
     }
-
     componentDidMount() {
         console.log("compo");
         let api_sensor = new API_sensor();
         api_sensor.fetchSensors().then(response => {
-            this.setState({totalSensor : response.data.sensor.length});
+            console.log("TAILLE");
+            this.state.totalSensor = response.data.sensors.length;
+            this.state.boo = true;
         }).catch(onerror => {
         });
-
     }
 
-    render(){
-        return (
-            <div>
-                <Card style={{ width: '21rem' }} id = "UserSensors">
+    render() {
+        if (this.boo == false) {
+            return (
+                <div>
+                    <Card style={{width: '21rem'}} id="UserSensors">
 
-                    <Card.Body>
-                        <Card.Title>Chiffres clés de l'entreprise</Card.Title>
-                        <Card.Text>
-                            <p>Nombre d'utilisateur : {this.state.totalUser}</p>
-                            <p>Nombre de capteur en service : {this.state.totalSensor}</p>
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Valeurs actualisées en live</small>
-                    </Card.Footer>
-                </Card>
-            </div>
-        );
-    }
+                        <Card.Body>
+                            <Card.Title>Chiffres clés de l'entreprise</Card.Title>
+                            <Card.Text>
+                                <p>Nombre d'utilisateur : </p>
+                                <p>Nombre de capteur en service : 0</p>
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Valeurs actualisées en live</small>
+                        </Card.Footer>
+                    </Card>
+                </div>
+            );
+
+    }else {
+            return (
+                <div>
+                    <Card style={{width: '21rem'}} id="UserSensors">
+
+                        <Card.Body>
+                            <Card.Title>Chiffres clés de l'entreprise</Card.Title>
+                            <Card.Text>
+                                <p>Nombre d'utilisateur : </p>
+                                <p>Nombre de capteur en service : {this.state.totalSensor}</p>
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Valeurs actualisées en live</small>
+                        </Card.Footer>
+                    </Card>
+                </div>
+            );
+        }
+
+}
 }
 export default UserSensors;
